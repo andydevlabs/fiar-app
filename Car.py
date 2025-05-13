aa4e5fec5e458b6eb7a1cb57152c92cc1a88db00
andydevlabs <avotiana@mail.hei.school>
2025-05-13 19:58:21 +0300

    feat: create car model

---
+++ Car.py
@@ -0,0 +1,7 @@
+class Car:
+    def __init__(self, car_id, model, seats, price_per_day, is_available):
+        self.car_id = car_id
+        self.model = model
+        self.seats = seats
+        self.price_per_day = price_per_day
+        self.is_available = is_available
