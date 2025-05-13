293aa3110010cac438b13f6466b9e979c04bf54e
andydevlabs <avotiana@mail.hei.school>
2025-05-13 14:55:05 +0300

    create car and rental class

---
+++ CarClass.js
@@ -0,0 +1,10 @@
+class CarClass {
+    constructor(carId, model, seats, isAvailable, pricePerDay, reservation) {
+        this.carId = carId;
+        this.model = model;
+        this.seats = seats;
+        this.isAvailable = isAvailable;
+        this.pricePerDay = pricePerDay;
+        this.reservation = reservation;
+    }
+}
---
+++ RentalClass.js
@@ -0,0 +1,9 @@
+class RentalClass {
+    constructor(rentalId, customerName, rentalStart, rentalEnd, carId) {
+        this.rentalId = rentalId;
+        this.customerName = customerName;
+        this.rentalStart = rentalStart;
+        this.rentalEnd = rentalEnd;
+        this.carId = carId;
+    }
+}
