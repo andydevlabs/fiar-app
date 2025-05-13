bbd07f0491d9e4e38c475a7aa41f46827d047992
andydevlabs <avotiana@mail.hei.school>
2025-05-13 19:46:29 +0300

    feat: change carService into typescript

--- carService.ts
+++ carService.ts
@@ -1,29 +1,38 @@
 import Car from "./Car.js";
 
-const carList = [
+interface RentalResult {
+    success: boolean;
+    message: string;
+}
+
+const carList: Car[] = [
     new Car(1, "Mercedes", 5, 150000, true),
     new Car(2, "Mitsubishi", 5, 120000, true),
     new Car(3, "Mazda", 9, 130000, true),
     new Car(4, "Kia", 5, 100000, true),
 ];
 
-export function getAllCars() {
+export function getAllCars(): Car[] {
     return carList;
 }
 
-export function getCarById(id) {
+export function getCarById(id: number): Car | undefined {
     return carList.find((car) => car.carId === id);
 }
 
-
-export function setCarAvailability(id, availability) {
+export function setCarAvailability(id: number, availability: boolean): void {
     const car = getCarById(id);
     if (car) {
         car.isAvailable = availability;
     }
 }
 
-export function rentCar(carId, renterName, duration, payment) {
+export function rentCar(
+    carId: number,
+    renterName: string,
+    duration: number,
+    payment: number
+): RentalResult {
     const car = getCarById(carId);
 
     if (!car || !car.isAvailable) {
